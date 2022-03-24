176c5efcaa78ca4900f8ea4f5b6b2f319fdc6468
Huan Nguyen <nguyentranghuan638@gmail.com>
2022-03-24 10:31:33 +0700

    video 4 cai dat giao dien admin dang nhap

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<link rel="stylesheet" href="{{ asset('template/admin/plugins/jquery/jquery.min.js') }}">
+<!-- Bootstrap 4 -->
+<link rel="stylesheet" href="{{ asset('template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
+
+<!-- AdminLTE App -->
+<link rel="stylesheet" href="{{ asset('template/admin/dist/js/adminlte.min.js') }}">
